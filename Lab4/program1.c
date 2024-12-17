98f015de153cc2c596a4954dafec6e8f2261de9f
freymi1856 <maksimpavlusin6@gmail.com>
2024-12-17 13:57:02 +0300

    Program1_Fix

--- Lab4/program1.c
+++ Lab4/program1.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
-#include "libgcf.h"
-#include "liblog.h"
+#include "libgcf.h"     // Заголовочный файл для функций НОД
+#include "liblog.h"     // Заголовочный файл для функций расчета e
 
 int main() {
     int command;
     printf("Введите команду: ");
-    scanf("%d", &command);
-
-    if (command == 1) {
-        int a, b;
-        printf("Введите два числа: ");
-        scanf("%d %d", &a, &b);
-        printf("НОД(%d, %d) = %d\n", a, b, GCF(a, b));
-    } else if (command == 2) {
-        int x;
-        printf("Введите x: ");
-        scanf("%d", &x);
-        printf("E(%d) = %.6f\n", x, E(x));
-    } else {
-        printf("Неизвестная команда\n");
+    
+    while (scanf("%d", &command) != EOF) {
+        if (command == 1) { // Подсчет НОД
+            int a, b;
+            scanf("%d %d", &a, &b);
+            printf("НОД(%d, %d) = %d\n", a, b, GCF(a, b));
+        } else if (command == 2) { // Расчет числа e
+            int x;
+            scanf("%d", &x);
+            printf("E(%d) = %f\n", x, E(x));
+        } else {
+            printf("Неизвестная команда. Используйте 1 для GCF и 2 для E.\n");
+        }
+        printf("Введите команду: ");
     }
-
+    
     return 0;
 }
