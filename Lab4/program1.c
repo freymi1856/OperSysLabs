4f4103015916bac6d5a2532ac01844fb25849d4b
freymi1856 <maksimpavlusin6@gmail.com>
2024-12-17 18:13:23 +0300

    Program1_Mod1

--- Lab4/program1.c
+++ Lab4/program1.c
@@ -3,24 +3,40 @@
 #include "liblog.h"
 
 int main() {
-    int command;
-    printf("Введите команду: ");
+    char command[256];
+    printf("Команды:\n");
+    printf("1 <int> <int> для подсчета НОД.\n");
+    printf("2 <int> для подсчета E.\n");
+    printf("q для выхода.\n");
+    printf("\n");
 
-    while (scanf("%d", &command) != EOF) {
-        if (command == 1) { // Подсчет НОД
+    while (1) {
+        printf("Введите команду: ");
+        fgets(command, sizeof(command), stdin);
+
+        if (command[0] == '1') { // Подсчет НОД
             int a, b;
-            scanf("%d %d", &a, &b);
+            if (sscanf(command, "1 %d %d", &a, &b) != 2){
+                printf("Некорректный ввод. Введите: 1 <int> <int>\n\n");
+                continue;
+            }
             printf("НОД (%d, %d) - Евклид: %d\n", a, b, GCF_EUCLID(a, b));
             printf("НОД (%d, %d) - Наивный: %d\n", a, b, GCF_NATIVE(a, b));
-        } else if (command == 2) { // Расчет числа e
+            printf("\n");
+        } else if (command[0] == '2') { // Расчет числа e
             int x;
-            scanf("%d", &x);
+            if (sscanf(command, "%d", &x) != 1){
+                printf("Некорректный ввод. Введите: 2 <int>\n\n");
+                continue;
+            }
             printf("E(%d) - По формуле: %f\n", x, E_FORMULA(x));
             printf("E(%d) - По сумме рядов: %f\n", x, E_SUMM(x));
+            printf("\n");
+        } else if (command[0] == 'q'){ // Выход
+            break;
         } else {
-            printf("Неизвестная команда. Используйте 1 для GCF и 2 для E.\n");
+            printf("Неизвестная команда. Используйте 1 для НОД и 2 для E.\n\n");
         }
-        printf("Введите команду: ");
     }
     printf("\n");
     
